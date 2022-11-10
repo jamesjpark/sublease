3afd9d15e5101be4c80b41e76fc1f8601a9c090a
Hyokwon Chung <hyochung@utexas.edu>
2022-11-09 22:24:58 -0600

    fixed auth endpoints

--- auth/urls.py
+++ auth/urls.py
@@ -4,9 +4,9 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 
 urlpatterns = [
-    path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', RegisterView.as_view(), name='auth_register'),
-    path('getUsers/', GetUsersView.as_view(), name='get_users'),
+    path('login', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register', RegisterView.as_view(), name='auth_register'),
+    path('getUsers', GetUsersView.as_view(), name='get_users'),
 
 ]
