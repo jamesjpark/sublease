f34d80f9540ce6143f06bdb87de605e7170f9893
Hyokwon Chung <hyochung@utexas.edu>
2022-11-12 01:07:58 -0600

    profile update and password change

--- auth/urls.py
+++ auth/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from auth.views import MyObtainTokenPairView, RegisterView, GetUsersView
+from auth.views import ChangePasswordView, MyObtainTokenPairView, RegisterView, GetUsersView, UpdateProfileView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 
 urlpatterns = [
-    path('login', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register', RegisterView.as_view(), name='auth_register'),
-    path('getUsers', GetUsersView.as_view(), name='get_users'),
-
+    path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='auth_register'),
+    path('getUsers/', GetUsersView.as_view(), name='get_users'),
+    path('change_password/<int:pk>/', ChangePasswordView.as_view(), name='auth_change_password'),
+    path('update_profile/<int:pk>/', UpdateProfileView.as_view(), name='auth_update_profile'),
 ]
